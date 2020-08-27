afefe431f69293451eb9eb10f1611c7950d34ae6
Josh <joshpiersimoni@gmail.com>
2020-08-27 11:50:14 -0400

    DbContext defined, Connection string added, Attributes added to Data Model

--- GeneralStoreAPI/Models/Transaction.cs
+++ GeneralStoreAPI/Models/Transaction.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,18 @@ namespace GeneralStoreAPI.Models
 {
     public class Transaction
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+        [Required]
+        [ForeignKey("Product")]
+        public string ProductSKU { get; set; }
+        public virtual Product Product { get; set; }
+        [Required]
+        public int ItemCount { get; set; }
+        public DateTime DateOfTransaction { get; set; }
     }
 }
